c381b6025390df2d8d17fec332086867d5634e60
sunny bharne <sunny.bharne@outlook.com>
2025-10-25 13:16:05 +0300

    added cli with copilot

--- lua/copilotcli/init.lua
+++ lua/copilotcli/init.lua
@@ -1,14 +1,16 @@
 local M = {}
 
-function M.hello()
+function M.open_copilot()
   vim.cmd('vsplit | wincmd l')
   vim.cmd('vertical resize 30')
-  vim.cmd('terminal')
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(0, buf)
+  vim.fn.termopen('copilot')
   vim.wo.winfixwidth = true
 end
 
 function M.setup()
-  vim.api.nvim_create_user_command('Hello', M.hello, {})
+  vim.api.nvim_create_user_command('Cocli', M.open_copilot, {})
 end
 
 return M
