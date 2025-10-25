dacc01501f7e9b54c0f97eb2f03ac3a9b96b8d82
sunny bharne <sunny.bharne@outlook.com>
2025-10-25 13:12:46 +0300

    added terminal

--- lua/copilotcli/init.lua
+++ lua/copilotcli/init.lua
@@ -3,9 +3,7 @@ local M = {}
 function M.hello()
   vim.cmd('vsplit | wincmd l')
   vim.cmd('vertical resize 30')
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(0, buf)
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Hello World" })
+  vim.cmd('terminal')
   vim.wo.winfixwidth = true
 end
 
